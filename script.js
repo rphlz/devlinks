e490672cd38f0b367ad3742e11a22432c298973e
Raphael Díaz <raphaeldiaz@outlook.com>
2023-08-31 12:31:50 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+  const html = document.documentElement
+
+  //  if (html.classList.contains("light")) {
+  //    html.classList.remove("light")
+  //  } else {
+  //    html.classList.add("light")
+  //  }
+
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de Mayk Brito sorrindo, usando óculos escuros e jaqueta de couro preta, sem barba e fundo em gradiente de azul e rosa"
+    )
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
